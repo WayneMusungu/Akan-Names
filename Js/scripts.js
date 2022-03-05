d8ce08b4da630dfeed21c9086129ce83a4e36801
WayneMusungu <waynemuyera17@gmail.com>
2022-03-05 09:59:51 +0300

    Remove value of Female and Male

---
+++ Js/scripts.js
@@ -0,0 +1,13 @@
+// Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+//  where;
+
+//  CC - is the century digits. For example 1989 has CC = 19
+
+//  YY - is the Year digits (1989 has YY = 89)
+
+//  MM -  is the Month
+
+//  DD - is the Day of the month 
+
+//  mod - is the modulus function ( % )
