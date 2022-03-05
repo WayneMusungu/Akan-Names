d2454c1a7c4e6a5ab40da680b544a78d881f452e
WayneMusungu <waynemuyera17@gmail.com>
2022-03-05 21:15:02 +0300

    Add akan name of male, female and days of the week

--- Js/scripts.js
+++ Js/scripts.js
@@ -39,4 +39,12 @@ function click(){
      d=parseInt(d)
      console.log(d)
 
+
+    var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+    
+
 }
