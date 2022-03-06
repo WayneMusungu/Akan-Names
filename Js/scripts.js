30992ba784738afe097af4dffc4eaf0358f4fd54
WayneMusungu <waynemuyera17@gmail.com>
2022-03-06 12:49:54 +0300

    Add male.chekced and female.checked

--- Js/scripts.js
+++ Js/scripts.js
@@ -14,15 +14,13 @@
 
 // [1900 12 31]- Our calendar year example
 
-
-function click(){
+function done(){
     var dob = document.getElementById("dob");
     var male = document.getElementById("male");
     var female = document.getElementById("female");
     dobArray = dob.value.split("-")
     
     console.log(dobArray)
-
     var CC = dobArray[0][0] + dobArray[0][1]
     console.log(CC)
 
@@ -30,21 +28,28 @@ function click(){
     console.log(YY)
 
     var MM =dobArray[1]
-    console.log[MM]
+    console.log(MM)
 
     var DD = dobArray[2]
     console.log(DD)
 
     var d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-     d=parseInt(d)
-     console.log(d)
-
-
+    d=parseInt(d)
+    console.log(d)
+    
     var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
     var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
     var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 
-    
+    if(male.checked === true){
+        console.log(akanMale[d])
+        alert("Your Akan Name is " + akanMale[d])
+    }
 
+    if(female.checked === true){
+        console.log(akanFemale[d])
+
+        alert("Your Akan Name is " + akanFemale[d])
+    }
 }
